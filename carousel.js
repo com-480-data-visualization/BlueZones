6e535b1c9712ab24b3967ae28ed1943ecefd1ab9
ioannapoulou <ioannapoulou08@gmail.com>
2026-04-29 12:34:35 +0200

    Added automated rotation in the globe

--- carousel.js
+++ carousel.js
@@ -3,13 +3,40 @@
 
 const wrapperNode = document.querySelector('.embla')
 const viewportNode = wrapperNode.querySelector('.embla__viewport')
-const prevButtonNode = wrapperNode.querySelector('.embla__prev')
+const prevButtonNode = wrapperNode.querySelector('.emblaprev')
 const nextButtonNode = wrapperNode.querySelector('.embla__next')
 
 const emblaApi = EmblaCarousel(viewportNode, { loop: true }, [
     EmblaCarouselAutoplay({ delay: 5000, stopOnInteraction: false, stopOnMouseEnter: true })
 ])
 
+const slideToCountry = [
+    "Japan",
+    "Greece",
+    "Italy",
+    "Costa Rica",
+    "United States of America"
+]
+
+const notifyActiveCountry = () => {
+    const activeSlideIndex = emblaApi.selectedScrollSnap()
+    const activeCountry = slideToCountry[activeSlideIndex]
+
+    if (!activeCountry) return
+
+    window.currentBlueZoneCountry = activeCountry
+
+    window.dispatchEvent(
+        new CustomEvent("bluezone:active-country-change", {
+            detail: { country: activeCountry, slideIndex: activeSlideIndex }
+        })
+    )
+}
+
 setupTweenScale(emblaApi)
+
+emblaApi.on("select", notifyActiveCountry)
+emblaApi.on("reInit", notifyActiveCountry)
+notifyActiveCountry()
 
 emblaApi.plugins().autoplay?.play()
